4f68765f45e6751b0cf38d524c693b636d92a601
Gael Miranday <gaelmiranday08@gmail.com>
2023-10-06 13:39:19 +0200

    first_commit

---
+++ script.js
@@ -0,0 +1,27 @@
+function clearScreen() {
+    document.getElementById("result").value = "";
+    }
+    
+    function display(value) {
+    document.getElementById("result").value += value;
+    }
+    
+    
+    function calculate() {
+    let p = document.getElementById("result").value;
+    let q = eval(p);
+    document.getElementById("result").value = q;
+    }
+
+function clearScreen() {
+     document.getElementById("result").value = "";
+    }
+ function display(value) {
+    document.getElementById("result").value += value;
+    }
+
+    function calculate() {
+        let p = document.getElementById("result").value;
+        let q = eval(p);
+        document.getElementById("result").value = q;
+        }
